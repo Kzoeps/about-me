dac20c5c3bb4d46109b1c0e64cab016ee509553b
kzoeps <kzoepa@gmail.com>
2024-11-10 12:46:27 -0500

    create bop stop

---
+++ bop-stop/artists.js
@@ -0,0 +1,48 @@
+export const artists = [
+            {
+                "name": "Dedrik",
+                "coordinates": [
+                    27.439583419049203,
+                    89.67041015625001
+                ],
+                "markerImage": "https://i1.sndcdn.com/avatars-sseIcU78yXe2Ce90-nEdTzw-t240x240.jpg",
+                "cardImage": "https://i1.sndcdn.com/avatars-sseIcU78yXe2Ce90-nEdTzw-t240x240.jpg",
+                "songs": [
+                    {
+                        "name": "sho",
+                        "url": "https://www.youtube.com/watch?v=2tnEAVAFhCk"
+                    },
+                    {
+                        "name": "mawongpa",
+                        "url": "https://www.youtube.com/watch?v=Qv26Q4Wq4Tk"
+                    },
+                    {
+                        "name": "gan na",
+                        "url": "https://www.youtube.com/watch?v=Iuycmwkbbr0"
+                    }
+                ]
+            },
+            {
+                "name": "Pelden Drubi Alien",
+                "coordinates": [
+                    26.863050921131567,
+                    89.39849853515626
+                ],
+                "markerImage": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmt1ax1d76vYnwn16O1FHDZMTrLZDDNUmYgw&s",
+                "cardImage": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmt1ax1d76vYnwn16O1FHDZMTrLZDDNUmYgw&s",
+                "songs": [
+                    {
+                        "name": "Zam Zim",
+                        "url": "https://soundcloud.com/ali-en-837563118/zam-zim-alien"
+                    },
+                    {
+                        "name": "Lab Shay",
+                        "url": "https://soundcloud.com/ali-en-837563118/lap-shay-alien-x-azha"
+                    },
+                    {
+                        "name": "Nakhay",
+                        "url": "https://soundcloud.com/jigme-namgyel/nahkay-jd-x-alien"
+                    }
+                ]
+            }
+        ];
